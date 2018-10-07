759267005866619a41921c56887726e001c91e6d
Jacob Feldman <jacobgfeld@gmail.com>
2018-10-07 16:37:08 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,10 @@
 def display_board(board)
-  puts 
+  puts " #{board} | #{board} | #{board} "
+  puts ""
+  puts " #{board} | #{board} | #{board} "
+  
+  puts " #{board} | #{board} | #{board} "
+  
 end
 
 
